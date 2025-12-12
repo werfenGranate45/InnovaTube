908d9ff207de54a29925ff05150f5f8292ed998e
werfenGranate45 <eduardo.agustin.cervantes@gmail.com>
2025-12-12 00:17:50 -0600

    Primer commit

---
+++ InnovaTubeBackend/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CampusController;
+use App\Http\Controllers\DoorAccessController;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
